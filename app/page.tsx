eff13d1e92dc905f5add5b444bd478bf8bf39e58
Giankarlo Rivera <girivera@gbox.adnu.edu.ph>
2025-06-21 08:48:31 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div>
+      <main>
         <h1>HoloMyth</h1>
         <div className="card">
           <img src="https://hololive.hololivepro.com/wp-content/uploads/2020/07/Takanashi-Kiara_list_thumb.png"></img>
@@ -31,7 +31,7 @@ export default function Home() {
           <p className="flavor-text">“Dead Beats, listen up! Your Mori's been working hard for you!”</p>
         </div>
 
-        
+
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
