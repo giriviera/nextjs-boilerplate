4cee0d358714a59f5ae531ef95b6baccf7a23237
Giankarlo Rivera <girivera@gbox.adnu.edu.ph>
2025-06-21 09:05:45 +0800

    more css changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="display">
       <main>
         <h1>HoloMyth</h1>
         <div className="card">
